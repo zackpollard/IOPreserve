506e3f04d313e3566147e12a875289e1cc18673f
Zack Pollard <zackpollard@ymail.com>
2014-11-14 20:56:40 +0000

    Changed type to a Runnable so the listener works Asynchronously from the rest of the server. Added a basic listener that passes connection to a new ConnectionHandler class for processing.

--- IOPreserve Server/src/pro/zackpollard/iopreserve/server/io/ConnectionListener.java
+++ IOPreserve Server/src/pro/zackpollard/iopreserve/server/io/ConnectionListener.java
@@ -1,19 +1,40 @@
 package pro.zackpollard.iopreserve.server.io;
 
+import pro.zackpollard.iopreserve.server.utils.Logger;
+
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Created by zack on 14/11/14.
  */
-public class ConnectionListener {
+public class ConnectionListener implements Runnable {
 
-    private ServerSocket serverSocket;
+    public void run() {
 
-    public ConnectionListener() {
+        ServerSocket serverSocket = null;
 
         try {
 
-            serverSocket = new ServerSocket()
+            serverSocket = new ServerSocket(8123);
+        } catch (IOException e) {
+
+            Logger.log(Logger.LoggerLevel.FATAL, "The server was not able to open the socket set in the config, is it already in use?", e);
+            System.exit(-1);
+        }
+
+        while(true) {
+
+            Socket socket = null;
+            try {
+
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+
+                Logger.log(Logger.LoggerLevel.ERROR, "There was an error when accepting the connection from the ServerSocket.", e);
+            }
+            new ConnectionHandler(socket);
         }
     }
 }
