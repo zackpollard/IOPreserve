2f159924735b41ec1e9eda5a9c2328f20bfc2ca3
Zack Pollard <zackpollard@ymail.com>
2014-11-21 16:16:02 +0000

    Changed authenticated check so it checks if not authenticated as intended. Added 403:Access Denied! message if not authenticated and tries to send a command other than AUTHCODE. Created the BACKUP command. Created the GETBACKUPLIST command.

--- IOPreserve Server/src/pro/zackpollard/iopreserve/server/io/ConnectionHandler.java
+++ IOPreserve Server/src/pro/zackpollard/iopreserve/server/io/ConnectionHandler.java
@@ -3,11 +3,9 @@ package pro.zackpollard.iopreserve.server.io;
 import pro.zackpollard.iopreserve.server.utils.DataStore;
 import pro.zackpollard.iopreserve.server.utils.Logger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class ConnectionHandler implements Runnable {
 
@@ -26,8 +24,8 @@ public class ConnectionHandler implements Runnable {
 
     public void run() {
 
-        PrintWriter out = null;
-        BufferedReader in = null;
+        PrintWriter out;
+        BufferedReader in;
 
         try {
 
@@ -38,11 +36,11 @@ public class ConnectionHandler implements Runnable {
 
             while ((inputLine = in.readLine()) != null && running) {
 
-                if (authenticated) {
+                if (!authenticated) {
 
-                    if (inputLine.startsWith("AUTHCODE:")) {
+                    if (inputLine.startsWith("AUTHCODE")) {
 
-                        String authCode = inputLine.split(":")[1];
+                        String authCode = in.readLine();
 
                         if (authedServers.contains(authCode)) {
 
@@ -52,10 +50,52 @@ public class ConnectionHandler implements Runnable {
 
                             out.println("401:Access Denied!");
                         }
+                    } else {
+
+                        out.println("403:Access Denied!");
                     }
                 } else {
 
-                    //Commands here!
+                    if (inputLine.startsWith("BACKUP")) {
+
+                        String folderPath = in.readLine();
+                        String fileName = in.readLine();
+
+                        File storeFile = new File("./backupStore" + File.separator + socket.getInetAddress().getHostAddress() + File.separator + folderPath);
+
+                        storeFile.mkdirs();
+
+                        storeFile = new File(storeFile + File.separator + fileName);
+                        storeFile.createNewFile();
+                        byte[] byteArray = new byte[1024];
+
+                        InputStream fileInput = socket.getInputStream();
+                        BufferedOutputStream fileOutput = new BufferedOutputStream(new FileOutputStream(storeFile));
+
+                        int bytesRead = fileInput.read(byteArray, 0, byteArray.length);
+                        fileOutput.write(byteArray, 0, bytesRead);
+                        fileOutput.close();
+
+                    } else if (inputLine.equals("GETBACKUPLIST")) {
+
+                        String folderPath = in.readLine();
+
+                        File folder = new File("./backupStore" + File.separator + socket.getInetAddress().getHostAddress() + File.separator + folderPath);
+
+                        File[] zipFiles = folder.listFiles(new FileFilter() {
+                            @Override
+                            public boolean accept(File file) {
+                                return (file.getName().endsWith(".zip") || file.getName().endsWith(".tar.gz"));
+                            }
+                        });
+
+                        for (File file : zipFiles) {
+
+                            out.println(file.getName());
+                        }
+
+                        out.println("200:List Sent!");
+                    }
                 }
             }
         } catch (IOException e) {
